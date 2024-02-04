e43e2e0e82d306e7d26a72f050d802ba0f2608be
Evgeniya D. <Evgeniya7000@rambler.ru>
2024-02-04 14:59:04 +1200

    initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,14 @@
+public class CreditPaymentService {
+    public int calculate(int timeMonth, int sumCreditRub, double ratePersent) {
+        double coeffInMonth; // Ставка по кредиту в месяц
+        double coeffAnnuity; // Коэффициент аннуитета
+        double paymantMonth; // Ежемесячный платеж
+        coeffInMonth = ratePersent/12/100;
+        coeffAnnuity = (coeffInMonth * Math.pow((1+coeffInMonth),timeMonth))/(Math.pow((1+coeffInMonth),timeMonth) - 1);
+        paymantMonth = sumCreditRub * coeffAnnuity;
+        return (int) paymantMonth;
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int timeMonth = 12;
+        int sumCreditRub = 1_000_000;
+        double ratePersent = 9.99;
+        int paymantMonth = service.calculate(timeMonth, sumCreditRub, ratePersent);
+        System.out.println(paymantMonth);
+
+        System.out.println();
+        System.out.println("12, 1_000_000, 9.99");
+        System.out.println(service.calculate(12, 1_000_000, 9.99));
+
+        System.out.println();
+        System.out.println("24, 1_000_000, 9.99");
+        System.out.println(service.calculate(24, 1_000_000, 9.99));
+
+        System.out.println();
+        System.out.println("36, 1_000_000, 9.99");
+        System.out.println(service.calculate(36, 1_000_000, 9.99));
+    }
+}
